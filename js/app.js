d2942eb39efa6bb2a9653244d7498d4bcbe0fbee
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-23 16:06:21 -0400

    hali develops website - start

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//   console.log('Ready!');
+//   $('#project-1 a').hover(function(){
+//     $('body').css('background','url(images/windows-1076116_1920.jpg)').css('color','#fff').css('border-bottom','1px solid #fff');
+//   }, function(){
+//     $('body').css('background','#fff').css('color','#000')
+//   });
+//
+//   $('#project-2 a').hover(function(){
+//     $('body').css('background','url(images/michiko.jpg) no-repeat center').css('background-size','cover').css('color','#fff')
+//   }, function(){
+//     $('body').css('background','#fff').css('color','#000')
+//   });
+// });
