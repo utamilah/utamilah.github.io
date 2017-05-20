934c72ebf6cfdc4913d02e78326739076db2c971
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-20 11:07:21 -0400

    add jquery to toggle project view

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function(e){
   $('.project h2 a').on('click', () => {
     event.preventDefault()
 
     let text = $(event.target).text();
     console.log(text)
 
-    $('.project-view').css('left', '100%')
+    $('.project-view').css('left', '57%')
   })
 
   $('.exit').on('click', () => {
-    $('.project-view').css('right', 0)
+    $('.project-view').css('left', '100%')
   })
-
 })
