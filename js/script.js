20c1c31f2dceb128b95bd9801ff7c481795a4bc3
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-08-09 01:05:06 -0400

    add mouseout event listener

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 // Text Shadow Mouse Moving Effect
 const myName = document.querySelector('.contact-info li:first-child')
-const wrapper = document.querySelector('.wrapper')
+const body = document.querySelector('body')
+const walk = 20;
 
 function shadow(e) {
-  console.log(e)
+  const {offsetWidth: width, offsetHeight: height} = body
+  let {offsetX: x, offsetY: y} = e
+
+  if(this !== e.target) {
+    x = x + e.target.offsetLeft
+    y = y + e.target.offsetTop
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2))
+  const yWalk = Math.round((y / width * walk) - (walk / 2))
+
+  myName.style.textShadow = `
+    ${yWalk}px ${xWalk}px 0 rgba(0, 0, 0, 0.3)
+  `
 }
 
 function resetShadow() {
-  console.log('mouseout')
+  myName.style.textShadow = `0 0 0 rgba(0,0,0,0.3)`
 }
 
-wrapper.addEventListener('mousemove', shadow)
-wrapper.addEventListener('mouseout', resetShadow)
+body.addEventListener('mousemove', shadow)
+window.addEventListener('mouseout', resetShadow)
