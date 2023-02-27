d2f63eefd42d3b31df9431af962cb9b6bf9f574d
Hali <halimatu.j.bakarr@gmail.com>
2023-02-27 14:23:46 +0000

    reformat using webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+	entry: "./src/app.js",
+	output: {
+		path: __dirname + "/dist/build",
+		filename: "bundle.js",
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "src"),
+		},
+		compress: true,
+		port: 8000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+		}),
+	],
+};
